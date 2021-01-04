ec2d7a3a60ec9d850609a6769ba752bdeacabfdf
David <sdavids10@gmail.com>
2021-01-03 20:52:25 -0500

    Primer commit

---
+++ js/megamenu.js
@@ -0,0 +1,9 @@
+ 
+      $(document).ready(function() {
+         
+        $(document).on('click', '.dropdown-menu', function (e) {
+          e.stopPropagation();
+        });
+        
+      
+    }); 
